73595c8d84f5d1802b8f5cd6595b76aa671493e5
Oreoluwa Ajayi <oreoluwaajayyiruth@gmail.com>
2023-10-13 23:32:19 +0100

    Detail Page feature

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -37,70 +37,145 @@ const Detail = () => {
    return (
      <div className="detail-container">
        <div className="detail-left-column">
-         <div className="part-one">
-           <h2>Filters</h2>
-           <button>Clear Filter</button>
-         </div>
+         <div className="">
+           <div className="part-one">
+             <h2>Filters</h2>
+             <button>Clear Filter</button>
+           </div>
+           <div className="part-two">
+             <div className="category">
+               <h2>Category</h2>
+               <FontAwesomeIcon icon={"angle-up"} />
+             </div>
+             <div className="lists">
+               <h3>CUSTOM PCS</h3>
+               <p>15</p>
+             </div>
+             <div className="lists">
+               <h3>MSI ALL-IN-ONE PCS</h3>
+               <p>45</p>
+             </div>
+             <div className="lists">
+               <h3>HP/COMPAQ PCS</h3>
+               <p>1</p>
+             </div>
+           </div>
+           <div className="part-two">
+             <div className="category">
+               <h2>Price</h2>
+               <FontAwesomeIcon icon={"angle-up"} />
+             </div>
+             <div className="lists">
+               <h3>$0.00 - $1,000.00</h3>
+               <p>19</p>
+             </div>
+             <div className="lists">
+               <h3>$0.00 - $1,000.00</h3>
+               <p>21</p>
+             </div>
+             <div className="lists">
+               <h3>$0.00 - $1,000.00</h3>
+               <p>9</p>
+             </div>
+             <div className="lists">
+               <h3>$0.00 - $1,000.00</h3>
+               <p>6</p>
+             </div>
+             <div className="lists">
+               <h3>$0.00 - $1,000.00</h3>
+               <p>3</p>
+             </div>
+             <div className="lists">
+               <h3>$0.00 - $1,000.00</h3>
+               <p>1</p>
+             </div>
+             <div className="lists">
+               <h3>$0.00 - $1,000.00</h3>
+               <p>1</p>
+             </div>
+             <div className="lists">
+               <h3>$0.00 - $1,000.00</h3>
+               <p>1</p>
+             </div>
+           </div>
+           <div className="part-three">
+             <div className="category">
+               <h2>Color</h2>
+               <FontAwesomeIcon icon={"angle-up"} />
+             </div>
+             <div className="red-black">
+               <div className="black"></div>
+               <div className="red"></div>
+             </div>
+           </div>
+           <div className="part-four">
+             <div className="category">
+               <h2>Filter Name</h2>
+               <FontAwesomeIcon icon={"angle-down"} />
+             </div>
+             <div className="button-div">
+               <button className="button1">Apply Filters (2)</button>
+             </div>
+           </div>
 
-         <div className="part-two">
-           <div className="category">
-             <h2>Category</h2>
-             <FontAwesomeIcon icon={"angle-up"} />
-           </div>
-           <div className="lists">
-             <h3>CUSTOM PCS</h3>
-             <p>15</p>
-           </div>
-           <div className="lists">
-             <h3>MSI ALL-IN-ONE PCS</h3>
-             <p>45</p>
-           </div>
-           <div className="lists">
-             <h3>HP/COMPAQ PCS</h3>
-             <p>1</p>
-           </div>
-         </div>
-         <div className="part-two">
-           <div className="category">
-             <h2>Price</h2>
-             <FontAwesomeIcon icon={"angle-up"} />
-           </div>
-           <div className="lists">
-             <h3>$0.00 - $1,000.00</h3>
-             <p>19</p>
-           </div>
-           <div className="lists">
-             <h3>$0.00 - $1,000.00</h3>
-             <p>21</p>
-           </div>
-           <div className="lists">
-             <h3>$0.00 - $1,000.00</h3>
-             <p>9</p>
-           </div>
-           <div className="lists">
-             <h3>$0.00 - $1,000.00</h3>
-             <p>6</p>
-           </div>
-           <div className="lists">
-             <h3>$0.00 - $1,000.00</h3>
-             <p>3</p>
-           </div>
-           <div className="lists">
-             <h3>$0.00 - $1,000.00</h3>
-             <p>1</p>
-           </div>
-           <div className="lists">
-             <h3>$0.00 - $1,000.00</h3>
-             <p>1</p>
-           </div>
-           <div className="lists">
-             <h3>$0.00 - $1,000.00</h3>
-             <p>1</p>
+           <div className="brand-div">
+            
            </div>
          </div>
        </div>
 
        <div className="detail-right-column">
+         <div className="products-container">
+           {laptops.map((laptop) => (
+             <div
+               onClick={() =>
+                 navigate("/desktops", {
+                   state: {
+                     id: laptop.id,
+                     img: laptop.img,
+                   },
+                 })
+               }
+               key={laptop.id}
+             >
+               <SeriesCard laptop={laptop} />
+             </div>
+           ))}
+         </div>
+         <div className="products-container">
+           {laptops.map((laptop) => (
+             <div
+               onClick={() =>
+                 navigate("/desktops", {
+                   state: {
+                     id: laptop.id,
+                     img: laptop.img,
+                   },
+                 })
+               }
+               key={laptop.id}
+             >
+               <SeriesCard laptop={laptop} />
+             </div>
+           ))}
+         </div>
+         <div className="products-container">
+           {laptops.map((laptop) => (
+             <div
+               onClick={() =>
+                 navigate("/desktops", {
+                   state: {
+                     id: laptop.id,
+                     img: laptop.img,
+                   },
+                 })
+               }
+               key={laptop.id}
+             >
+               <SeriesCard laptop={laptop} />
+             </div>
+           ))}
+         </div>
          <div className="products-container">
            {laptops.map((laptop) => (
              <div
--- src/components/DetailsNavbar.jsx
+++ src/components/DetailsNavbar.jsx
@@ -1,25 +1,37 @@
 
 import React from "react";
 import '../styles/LandingPage.scss'
+import paypal from '../assets/img/paypal.svg'
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown, faAngleUp, faSearch, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const DetailsNavbar = () => {
   return (
-    <div className="main-content">
-      <div className="navbar">        
-          <ul className="nav-links">
-            <li>About Product</li>
-            <li>Details</li>
-            <li>Specs</li>
-          </ul>                  
-        <div className="right">
-          <p>On Sale from $3,299.00</p>
-          <button className="button1">Add to Cart</button>
-          <button className="button2">PayPal</button>          
+    <div className="main-content1">
+      <div className="navbar">
+        <ul className="nav-links">
+          <li>About Product</li>
+          <li>Details</li>
+          <li>Specs</li>
+        </ul>
+      </div>
+      <div className="right">
+        <p>On Sale from $3,299.00</p>
+        <div className="one">
+          <p>1</p>
+          <div className="up-down">
+            <FontAwesomeIcon icon={faAngleUp} />
+            <FontAwesomeIcon icon={faAngleDown} />
+          </div>
         </div>
+        <button className="button1">Add to Cart</button>
+        <button className="button2">
+          <img src={paypal} alt="" />
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
 export default DetailsNavbar
